f466076a125f86b110e7064391680c2f7dbbd142
unknown <isyuhada19@gmail.com>
2020-11-27 20:31:27 +0700

    create style component

---
+++ components/atoms/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <button class="button" @click="$emit('click-button')" >{{ titleButton }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        titleButton:{
+            type: String,
+            default: 'submit'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
